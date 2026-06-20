5c630e63bfa16c6d79d32f07ef11af44ca878202
aluisack <aluiscueto1994@gmail.com>
2026-06-20 12:11:35 -0600

    eliminar ruta historia

--- app/api/alebrijes/[id]/historia/route.ts
+++
@@ -1,4 +0,0 @@
-import { NextRequest } from "next/server";
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-  return new Response("No disponible", { status: 503 });
-}
